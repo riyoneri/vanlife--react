d3c676dc593490832a88361d60e642c00760e3a7
Lionel Kaneza <lionkaneza@gmail.com>
2023-10-03 12:49:01 +0200

    fix: set fixed height for black background on homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
-    <div className="bg-hero bg-no-repeat bg-cover bg-bottom h-screen">
-      <div className="h-hero-screen bg-blend-overlay bg-black/50 text-white items-center justify-center gap-10 flex flex-col px-5 sm:px-10">
+    <div className="bg-hero bg-no-repeat bg-cover bg-bottom">
+      <div className="h-screen bg-blend-overlay bg-black/50 text-white items-center justify-center gap-10 flex flex-col px-5 sm:px-10">
         <p className="text-xl sm:text-4xl lg:text-5xl font-bold">
           You got the travel plans, we got the travel vans.
         </p>
