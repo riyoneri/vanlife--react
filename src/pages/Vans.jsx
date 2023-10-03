45df17c47bbfa0416b0147adc20dde2a0c77e5f0
Lionel Kaneza <lionkaneza@gmail.com>
2023-10-03 19:29:48 +0200

    refactor: format vans page component

--- src/pages/Vans.jsx
+++ src/pages/Vans.jsx
@@ -13,21 +13,38 @@ export default function Vans() {
   }, []);
 
   const cardDisplay = data.map((item) => (
-    <Card style="max-h-[25rem] sm:h-auto" key={item.id} {...item} buttonColor="red" />
+    <Card
+      style="max-h-[25rem] w-full sm:h-auto"
+      key={item.id}
+      {...item}
+      buttonColor="red"
+    />
   ));
 
   return (
     <div className="px-5 sm:px-10 mb-10">
-      <h1 className="text-xl sm:text-3xl font-bold mt-10">Explore our van options</h1>
+      <h1 className="text-xl sm:text-3xl font-bold mt-10">
+        Explore our van options
+      </h1>
       <div className="mt-5 flex justify-between items-center">
-        <div className="flex gap-2 sm:gap-5 mr-5 sm:mr-0">
-          <Button style="sm:px-5 sm:py-2 sm:text-base text-sm px-2">Simple</Button>
-          <Button style="sm:px-5 sm:py-2 sm:text-base text-sm px-2">Luxury</Button>
-          <Button style="sm:px-5 sm:py-2 sm:text-base text-sm px-2">Rugged</Button>
+        <div className="flex gap-2 sm:gap-5">
+          <Button style="sm:px-5 sm:py-2 sm:text-base text-sm px-2">
+            Simple
+          </Button>
+          <Button style="sm:px-5 sm:py-2 sm:text-base text-sm px-2">
+            Luxury
+          </Button>
+          <Button style="sm:px-5 sm:py-2 sm:text-base text-sm px-2">
+            Rugged
+          </Button>
         </div>
-        <button className="underline underline-offset-2">Clear filters</button>
+        <button className="underline underline-offset-2 text-sm sm:text-base">
+          Clear filters
+        </button>
       </div>
-      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-10 gap-10">{cardDisplay}</div>
+      <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-10 gap-10">
+        {cardDisplay}
+      </div>
     </div>
   );
 }
