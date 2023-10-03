766744982753fa5caed2397003a7f501307fe2d8
Lionel Kaneza <lionkaneza@gmail.com>
2023-10-03 08:42:00 +0200

    feat: finish homepage desktop design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,13 +5,17 @@ export default function Home() {
   return (
     <div className="bg-hero bg-no-repeat bg-cover bg-bottom h-screen">
       <Navigation />
-      <div className="h-hero-screen bg-red-500 flex flex-col">
-        <p>You got the travel plans, we got the travel vans.</p>
-        <p>
+      <div className="h-hero-screen bg-blend-overlay bg-black/50 text-white items-center justify-center gap-10 flex flex-col px-10">
+        <p className="text-5xl font-bold">
+          You got the travel plans, we got the travel vans.
+        </p>
+        <p className="text-xl">
           Add adventure to your life by joining the #vanlife movement. Rent the
           perfect van to make your perfect road trip.
         </p>
-        <Link className="bg-[#FF8C38]">Find your van</Link>
+        <Link className="bg-[#FF8C38] text-center px-32 py-2 rounded-sm font-semibold">
+          Find your van
+        </Link>
       </div>
     </div>
   );
