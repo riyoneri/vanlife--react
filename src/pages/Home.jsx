a9c07a7c01d55da45ca4f5dceb0b8f18e0271308
Lionel Kaneza <lionkaneza@gmail.com>
2023-10-03 08:52:00 +0200

    feat: home page responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,15 +6,15 @@ export default function Home() {
   return (
     <div className="bg-hero bg-no-repeat bg-cover bg-bottom h-screen">
       <Navigation />
-      <div className="h-hero-screen bg-blend-overlay bg-black/50 text-white items-center justify-center gap-10 flex flex-col px-10">
-        <p className="text-5xl font-bold">
+      <div className="h-hero-screen bg-blend-overlay bg-black/50 text-white items-center justify-center gap-10 flex flex-col px-5 sm:px-10">
+        <p className="text-xl sm:text-4xl lg:text-5xl font-bold">
           You got the travel plans, we got the travel vans.
         </p>
-        <p className="text-xl">
+        <p className="lg:text-xl">
           Add adventure to your life by joining the #vanlife movement. Rent the
           perfect van to make your perfect road trip.
         </p>
-        <Link className="bg-[#FF8C38] text-center px-32 py-2 rounded-sm font-semibold">
+        <Link className="bg-[#FF8C38] self-stretch sm:self-center text-center sm:px-32 py-2 rounded-sm font-semibold">
           Find your van
         </Link>
       </div>
