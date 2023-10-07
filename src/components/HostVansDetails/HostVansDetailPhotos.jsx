993a024cb70753ca6e8f1aee25a525f5d080408c
Lionel Kaneza <lionkaneza@gmail.com>
2023-10-07 19:11:23 +0200

    refactor: format HostVansDetailPhotos component

--- src/components/HostVansDetails/HostVansDetailPhotos.jsx
+++ src/components/HostVansDetails/HostVansDetailPhotos.jsx
@@ -2,10 +2,13 @@ import { useOutletContext } from "react-router-dom";
 
 export default function HostVansDetailPhotos() {
   const [currentVan, setCurrentVan] = useOutletContext();
-  console.log(currentVan)
+  console.log(currentVan);
   return (
     <div className="bg-white px-5 lg:w-4/5 mx-auto py-4 grid grid-cols-2 sm:grid-cols-4">
-      <img className="h-20 sm:h-32 rounded-md object-cover" src={currentVan.imageUrl} />
+      <img
+        className="h-20 sm:h-32 rounded-md object-cover"
+        src={currentVan.imageUrl}
+      />
     </div>
   );
 }
