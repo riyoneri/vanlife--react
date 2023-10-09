56d791dcc7815d8e136d6f8c2ff98f7a79e36e8c
Lionel Kaneza <lionkaneza@gmail.com>
2023-10-09 11:15:24 +0200

    feat: implement RouterProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 
 // Pages
 import Home from "./pages/Home";
@@ -19,33 +24,33 @@ import Layout from "./components/Layouts/MainLayout";
 import HostLayout from "./components/Layouts/HostLayout";
 import HostVansDetailLayout from "./components/Layouts/HostVansDetailLayout";
 
-export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
 
-          <Route path="vans">
-            <Route index element={<Vans />} />
-            <Route path=":id" element={<VansDetail />} />
-          </Route>
+      <Route path="vans">
+        <Route index element={<Vans />} />
+        <Route path=":id" element={<VansDetail />} />
+      </Route>
 
-          <Route path="host" element={<HostLayout />}>
-            <Route index element={<Host />} />
-            <Route path="income" element={<Income />} />
-            <Route path="vans" element={<HostVans />} />
-            <Route path="vans/:id" element={<HostVansDetailLayout />}>
-              <Route index element={<HostVansDetail />} />
-              <Route path="pricing" element={<HostVansDetailPricing />} />
-              <Route path="photos" element={<HostVansDetailPhotos />} />
-            </Route>
-            <Route path="reviews" element={<Reviews />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
+      <Route path="host" element={<HostLayout />}>
+        <Route index element={<Host />} />
+        <Route path="income" element={<Income />} />
+        <Route path="vans" element={<HostVans />} />
+        <Route path="vans/:id" element={<HostVansDetailLayout />}>
+          <Route index element={<HostVansDetail />} />
+          <Route path="pricing" element={<HostVansDetailPricing />} />
+          <Route path="photos" element={<HostVansDetailPhotos />} />
         </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+        <Route path="reviews" element={<Reviews />} />
+      </Route>
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+);
+
+export default function App() {
+  return <RouterProvider router={router} />;
 }
