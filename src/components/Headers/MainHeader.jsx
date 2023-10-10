ce8bd8bc5b6fe79621d5de31d8cd62062151b6bc
Lionel Kaneza <lionkaneza@gmail.com>
2023-10-10 17:22:48 +0200

    feat: add user icon svg

--- src/components/Headers/MainHeader.jsx
+++ src/components/Headers/MainHeader.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from "react-router-dom";
 import logoImg from "../../assets/images/logo.png";
+import userIcon from "../../assets/icons/user-circle.svg";
 
 export default function MainHeader() {
   return (
@@ -37,6 +38,16 @@ export default function MainHeader() {
           }
         >
           Vans
+        </NavLink>
+        <NavLink
+          to="/login"
+          className={({ isActive }) =>
+            `hover:underline underline-offset-2 hover:text-black ${
+              isActive ? "underline text-black" : ""
+            }`
+          }
+        >
+          <img src={userIcon} />
         </NavLink>
       </div>
     </nav>
