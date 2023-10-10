ce52e0e5f06b89834b1d1eb8920e22242bb9d73a
Lionel Kaneza <lionkaneza@gmail.com>
2023-10-10 17:41:51 +0200

    feat: handle login submit

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -5,35 +5,43 @@ import Button from "../components/Button/Button";
 export default () => {
   const [formData, setFormData] = useState({ email: "", password: "" });
 
-  function inputChangeHandler(event) {
+  function handleChange(event) {
     const { name, value } = event.target;
     setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
   }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+  }
+
   return (
     <div className="h-screen grid items-center">
-      <div className="grid gap-5 items-stretch max-w-xl w-full md:w-1/2 lg:w-1/3 px-5 mx-auto">
+      <form
+        onSubmit={handleSubmit}
+        className="grid gap-5 items-stretch max-w-xl w-full md:w-1/2 lg:w-1/3 px-5 mx-auto"
+      >
         <p className="text-xl font-bold text-center">Sign in to your account</p>
         <div className="grid gap-4">
           <input
             type="text"
             placeholder="Email Address"
-            className="py-1 px-2 rounded-sm focus:outline-0"
+            className="py-1 px-2 border-2 rounded-sm focus:outline-0"
             name="email"
-            onChange={inputChangeHandler}
+            onChange={handleChange}
             value={formData.email}
           />
           <input
             type="text"
             placeholder="Password"
-            className="py-1 px-2 rounded-sm focus:outline-0"
+            className="py-1 px-2 border-2 rounded-sm focus:outline-0"
             name="password"
-            onChange={inputChangeHandler}
+            onChange={handleChange}
             value={formData.password}
           />
         </div>
         <Button color="orange">Sign In</Button>
-      </div>
+      </form>
     </div>
   );
 };
