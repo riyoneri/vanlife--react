703dfd986dfa65933d10a176719895b2912095ec
Lionel Kaneza <lionkaneza@gmail.com>
2023-10-10 17:29:40 +0200

    feat: change handler for login inputs

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
+
 import Button from "../components/Button/Button";
 
 export default () => {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+
+  function inputChangeHandler(event) {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+  }
+
   return (
     <div className="h-screen grid items-center">
       <div className="grid gap-5 items-stretch w-1/3 px-5 mx-auto">
@@ -10,11 +19,17 @@ export default () => {
             type="text"
             placeholder="Email Address"
             className="py-1 px-2 rounded-sm focus:outline-0"
+            name="email"
+            onChange={inputChangeHandler}
+            value={formData.email}
           />
           <input
             type="text"
             placeholder="Password"
             className="py-1 px-2 rounded-sm focus:outline-0"
+            name="password"
+            onChange={inputChangeHandler}
+            value={formData.password}
           />
         </div>
         <Button color="orange">Sign In</Button>
