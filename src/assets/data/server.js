e38431c433eb9d3235bb9b7c48224852de144b47
Lionel Kaneza <lionkaneza@gmail.com>
2023-10-11 16:58:21 +0200

    feat: add authentication function in mirage

--- src/assets/data/server.js
+++ src/assets/data/server.js
@@ -72,6 +72,12 @@ createServer({
       type: "rugged",
       hostId: "123",
     });
+    server.create("user", {
+      id: "123",
+      email: "b@b.com",
+      password: "p123",
+      name: "Bob",
+    });
   },
 
   routes() {
@@ -97,6 +103,24 @@ createServer({
       // Hard-code the hostId for now
       const id = request.params.id;
       return schema.vans.where({ id, hostId: "123" });
+    });
+
+    this.post("/login", (schema, request) => {
+      const { email, password } = JSON.parse(request.requestBody);
+      const foundUser = schema.users.findBy({ email, password });
+      if (!foundUser) {
+        return new Response(
+          401,
+          {},
+          { message: "No user with those credentials found!" }
+        );
+      }
+
+      foundUser.password = undefined;
+      return {
+        user: foundUser,
+        token: "Enjoy your pizza, here's your tokens.",
+      };
     });
   },
 });
